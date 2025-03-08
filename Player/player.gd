8fbcae427affcc3641031380d56e9112b5e3883d
Max Estrella <maxestrella565@gmail.com>
2025-03-08 20:35:33 +0000

    Player sprite flips

--- Player/player.gd
+++ Player/player.gd
@@ -4,7 +4,8 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 const SPEED = 350.0
 const JUMP_VELOCITY = -500.0
 
-#MOVEMENT
+@onready var sprite = $Player_Sprite
+
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -16,4 +17,13 @@ func _physics_process(delta):
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
 	move_and_slide()
+	flip_sprite()
+
+func flip_sprite():
+	var mouse_pos = get_global_mouse_position()
+	if mouse_pos.x > global_position.x:
+		sprite.flip_h = true
+	else:
+		sprite.flip_h = false
